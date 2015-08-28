183033bcfd409ffaa3ac7f9e72469bf46baa6103
Frédéric Landry <fred@fullmetalpacket.com>
2015-08-28 09:58:39 -0400

    Test2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
 
     //MyTCPServer server;
-    //MyTCPClient client;rwartest
+    //MyTCPClient client;rwartestdsadasdasd
 
     //Listen connection requests coming from any IP to port 5001
 //    if(!server.listen(QHostAddress::Any,5001))
